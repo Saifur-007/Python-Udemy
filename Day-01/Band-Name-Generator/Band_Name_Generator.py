b9f5548fac7de555134c7baa61963a9bfe7f8bad
Saifur Rahman Alamin <101647817+Saifur-007@users.noreply.github.com>
2023-02-25 19:37:38 +0000

    Band_Name_Generator

---
+++ Day-01/Band-Name-Generator/Band_Name_Generator.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator.")
+
+city = input("Which city did you grow up in?\n") # I used \n to send my crosshair down.
+pet = input("WHat is the name of your Per?\n")
+
+print(" Your Band name could be : " , city , pet)
+
