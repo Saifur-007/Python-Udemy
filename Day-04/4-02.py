f7dc8ef4bd2596e8b6c1234a07b71eebb45e0887
Saifur Rahman Alamin <101647817+Saifur-007@users.noreply.github.com>
2023-03-04 19:17:46 +0000

    Day-04

---
+++ Day-04/4-02.py
@@ -0,0 +1,4 @@
+import random
+
+love_score = random.randint(1 ,100)
+print(f"Your love score is {love_score}.")
---
+++ Day-04/4-03.py
@@ -0,0 +1,11 @@
+# I am going to write a virtual coin toss program. It will randomly tell the user "Heads" or "Tails".
+
+
+import random
+
+random_side = random.randint(0,1)
+
+if random_side == 0:
+    print("Heads")
+else:
+    print("Tails")
